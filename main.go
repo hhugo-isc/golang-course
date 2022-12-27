3810f1fdb66ada893e458964c5f55f7672e6b09d
lhhernandez-isc <hhernandez.isc@gmail.com>
2022-12-27 11:20:58 -0600

    manejo de errores basico

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -20,9 +21,29 @@ func addValues(x, y int) (int, error) {
 	return x + y, nil
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var x, y, f float32
+	x = 100.0
+	y = 10.0
+	f, err := divideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", x, y, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		return 0, errors.New("Cannot divide by zero")
+	}
+	return x / y, nil
+}
+
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Printf("Iniciando aplicacion en el puerto %s", PortNumber)
 	_ = http.ListenAndServe(PortNumber, nil)
