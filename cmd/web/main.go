0a8f4efd8d4db76cc323147fd744b918b28f0fce
lhhernandez-isc <hhernandez.isc@gmail.com>
2022-12-27 14:25:12 -0600

    Se agrega el cache de los templates a la configuracion global

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,12 +4,24 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/lhhernandez-isc/golang-basis/pkg/config"
 	"github.com/lhhernandez-isc/golang-basis/pkg/handlers"
+	"github.com/lhhernandez-isc/golang-basis/pkg/render"
 )
 
 const PortNumber = ":8080"
 
 func main() {
+
+	var app config.AppConfig
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		fmt.Println("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	render.NewTemplates(&app)
+
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
