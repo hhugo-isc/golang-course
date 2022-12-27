37c2cfdea1cbd4aa9a93790e665f16373c1311f3
lhhernandez-isc <hhernandez.isc@gmail.com>
2022-12-27 11:53:14 -0600

    se reorganizan los archivos en la aplicacion

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/lhhernandez-isc/golang-basis/pkg/handlers"
 )
 
 const PortNumber = ":8080"
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Iniciando aplicacion en el puerto %s", PortNumber)
 	_ = http.ListenAndServe(PortNumber, nil)
