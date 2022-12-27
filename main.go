10b005442e04bd55d5b169e8d0d7c8f0d9e9af2f
lhhernandez-isc <hhernandez.isc@gmail.com>
2022-12-27 11:13:36 -0600

    aplicacion http basica configurada

--- main.go
+++ main.go
@@ -2,32 +2,28 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/lhhernandez-isc/golang-basis/helpers"
+	"net/http"
 )
 
-func main() {
-	// basis.Variables()
-	// basis.Pointers()
-	// basis.Structs()
-	// basis.StructsWithFuncs()
-	// basis.Maps()
-	// basis.Slices()
-	// basis.DesicionStruct()
-	// basis.SwitchStruct()
-	// basis.ForRangeStructure()
-	// basis.ForRangeStructureInMap()
-	// basis.ForRangeOverString()
-	// basis.ForRangeOverStruct()
-	// basis.Interfaces()
-	// basis.Channels()
-	// basis.ReadingJson()
-	// basis.WritingJson()
-	result, err := helpers.Divide(100.0, 0.0)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+const PortNumber = ":8080"
 
-	fmt.Println("Result of division is", result)
+func Home(w http.ResponseWriter, r *http.Request) {
+	_, _ = fmt.Fprintf(w, "This is the home page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum, _ := addValues(2, 3)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page, and 2 + 3 is %d", sum))
+}
+
+func addValues(x, y int) (int, error) {
+	return x + y, nil
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	fmt.Printf("Iniciando aplicacion en el puerto %s", PortNumber)
+	_ = http.ListenAndServe(PortNumber, nil)
 }
